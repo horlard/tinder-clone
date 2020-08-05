9040e226942ddbd2e6bf9cc97f446ee67c205a21
horlard <oladipuposaulih@gmail.com>
2020-08-05 14:04:42 +0100

    update

---
+++ src/TinderCards.js
@@ -0,0 +1,35 @@
+import React,{useState} from 'react'
+import TinderCard from 'react-tinder-card'
+import './TinderCard.css';
+
+export default function TinderCards() {
+    const [people,setPeople] = useState([
+        {
+            name: 'Steve Jobs',
+            url: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTY2MzU3OTcxMTUwODQxNTM1/steve-jobs--david-paul-morrisbloomberg-via-getty-images.jpg'
+        },
+        {
+            name: 'Mark Zukerburg',
+            url: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTQyMDA0NDgwMzUzNzcyNjA2/mark-zuckerberg_gettyimages-512304736jpg.jpg'
+        }
+    ])
+    return (
+        <div>
+            <h1>tinder Cards</h1>
+            <div className="card__container">
+            {
+                people.map(person=> {
+                    return (
+                    <TinderCard className='swipe' key={person.name} preventSwipe={['up','down']}>
+                        <div className="card" style={{backgroundImage: `url(${person.url})`,backgroundRepeat:'no-repeat'}}>
+                            <h3>{person.name}</h3>
+                        </div>
+                    </TinderCard>
+                    )
+                })
+            }
+            </div>
+            
+        </div>
+    )
+}
--- src/header.js
+++ src/header.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
+import IconButton from '@material-ui/core/IconButton'
+import './header.css'
 
 export default function header() {
     return (
-        <div>
-            <PersonIcon/>
+        <div className='header'>
+            <IconButton>
+                <PersonIcon fontSize='large' className='header__icon'/>
+            </IconButton>
+            <img className='header__logo' src='https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png' alt='tinder-logo'/>
+            <IconButton>
+                <ForumIcon fontSize='large' className='header__icon'/>
+            </IconButton>
+            
         </div>
     )
 }
