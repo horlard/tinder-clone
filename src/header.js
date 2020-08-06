6b80a4bdbe78432ad792b7f72623f002e469941c
horlard <oladipuposaulih@gmail.com>
2020-08-06 01:46:13 +0100

    chore: wrapped icons in links

--- src/header.js
+++ src/header.js
@@ -1,19 +1,38 @@
 import React from 'react'
 import PersonIcon from '@material-ui/icons/Person';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ForumIcon from '@material-ui/icons/Forum';
 import IconButton from '@material-ui/core/IconButton'
+import {Link} from 'react-router-dom';
 import './header.css'
 
-export default function header() {
+export default function header({backButton}) {
     return (
         <div className='header'>
+
+            {
+                backButton ? <Link to='/'>
+                                <IconButton>
+                                    <ArrowBackIosIcon fontSize='large' className='header__icon'/>
+                                </IconButton>
+                            </Link> : <IconButton>
+                                    <PersonIcon fontSize='large' className='header__icon'/>
+                            </IconButton>
+            }
+            
+            <Link to='/'>
             <IconButton>
-                <PersonIcon fontSize='large' className='header__icon'/>
-            </IconButton>
             <img className='header__logo' src='https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png' alt='tinder-logo'/>
-            <IconButton>
-                <ForumIcon fontSize='large' className='header__icon'/>
             </IconButton>
+            </Link>
+            
+            
+            <Link to='/chats'>
+            <IconButton>
+                    <ForumIcon fontSize='large' className='header__icon'/>
+            </IconButton>
+            </Link>
+            
             
         </div>
     )
